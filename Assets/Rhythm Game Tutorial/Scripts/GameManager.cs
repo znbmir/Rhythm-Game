ade6ad0a9dd4bbf852064e32b0a02646bb07683f
Zeynab <znb.mir@gmail.com>
2019-05-21 13:22:42 +0430

    21 May 2019

---
+++ Assets/Rhythm Game Tutorial/Scripts/GameManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+  public AudioSource theMusic;
+  public bool startPlaying;
+  public BeatScroller theBS;
+
+  public static GameManager instance;
+  public int currentScore;
+  public int scorePerNote = 100;
+
+  public int currentMultiplier;
+  public int multiplierTracker;
+  public int[] multiplierThresholds;
+
+  public Text scoreText;
+  public Text multiText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      instance = this;
+      scoreText.text = "Score: 0";
+      currentMultiplier = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if(!startPlaying)
+      {
+        if(Input.anyKeyDown)
+        {
+          startPlaying = true;
+          theBS.hasStarted = true;
+
+          theMusic.Play();
+        }
+      }
+    }
+
+    public void NoteHit()
+    {
+      Debug.Log("Hit On Time");
+
+      if(currentMultiplier - 1 < multiplierThresholds.Length)
+      {
+        multiplierTracker++;
+        if(multiplierThresholds[currentMultiplier - 1] <= multiplierTracker)
+        {
+          multiplierTracker = 0;
+          currentMultiplier++;
+        }
+      }
+
+      multiText.text = "Multiplier: x" + currentMultiplier;
+
+      currentScore += scorePerNote * currentMultiplier;
+      scoreText.text = "Score " + currentScore;
+    }
+
+    public void NoteMissed()
+    {
+      Debug.Log("Missed Note");
+    }
+}
